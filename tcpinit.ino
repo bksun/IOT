530f3d2a1d003b28a3a3e2b8343f283be06b06ef
bksun <sunilsinghmbm@gmail.com>
2018-09-15 17:18:45 +0530

    just typo, changed A6->GSM

--- tcpinit.ino
+++ tcpinit.ino
@@ -1,16 +1,16 @@
 bool tcpinit( String APN, String SERVER, String PORT )
 {
-  A6command("AT+CIPSHUT", "OK", ""yy"", 1500, 1);
-  A6command("ATZ", "OK", "yy", 1500, 1);
-  A6command("ATE0", "OK", "yy", 1500, 1);
+  GSMcommand("AT+CIPSHUT", "OK", ""yy"", 1500, 1);
+  GSMcommand("ATZ", "OK", "yy", 1500, 1);
+  GSMcommand("ATE0", "OK", "yy", 1500, 1);
 
-  A6command("AT+CNMI=0, 0, 0, 0, 0", "OK", "yy", 1500, 1);
-  A6command(AT+CMGF=1, "OK", "yy", 1500, 1);
+  GSMcommand("AT+CNMI=0, 0, 0, 0, 0", "OK", "yy", 1500, 1);
+  GSMcommand(AT+CMGF=1, "OK", "yy", 1500, 1);
 
-  A6command("AT+CSTT = " + APN + "," + "\"\"" + "," + "\"\"", "OK", "yy", 1500, 1);//It sets GSM APN
-  A6command("AT+CIICR", "OK", "yy", 1500, 1); //bring up wireless connection
-  A6command("AT+CIFSR", "OK", ".", 1500, 1); //get local IP adress
-  if ( A6command("AT+CIPSTART = TCP," + SERVER + "," + PORT, "OK", "CONNECT", 5000, 2) == "OK")
+  GSMcommand("AT+CSTT = " + APN + "," + "\"\"" + "," + "\"\"", "OK", "yy", 1500, 1);//It sets GSM APN
+  GSMcommand("AT+CIICR", "OK", "yy", 1500, 1); //bring up wireless connection
+  GSMcommand("AT+CIFSR", "OK", ".", 1500, 1); //get local IP adress
+  if ( GSMcommand("AT+CIPSTART = TCP," + SERVER + "," + PORT, "OK", "CONNECT", 5000, 2) == "OK")
   {
     return true;
   }
